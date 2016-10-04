56c8b7e69e1b1cc53b1d467ff924485d15959f09
Liza Kaz <kazakovaem@gmail.com>
2016-10-05 03:23:56 +0700

    исправление косячного коммита

--- 10.py
+++ 10.py
@@ -1,15 +1,15 @@
 def r(n):
-    b=[]
+    t = []
     for i in range(n):
-        if i==0 or i==n-1:
-            b.append(1)
+        if i == 0 or i == n-1:
+            t.append(1)
         else:
-            k=r(n-1)
-            b.append(k[i-1]+k[i])
-    return b
+            k = r(n-1)
+            t.append(k[i-1]+k[i])
+    return t
 def triangle(x):
     for i in range(x):
         print(r(i+1))
     return ' '
-a=int(input())
+a = int(input())
 print(triangle(a+1))
